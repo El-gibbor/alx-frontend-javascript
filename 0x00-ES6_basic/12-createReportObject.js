4ebe9c6501e71409348957aeb0f6cfcafbfe711a
El-gibbor <chiagoziem11@yahoo.com>
2024-06-30 07:48:17 +0200

    created a report objectnode

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,19 @@
+import createEmployeesObject from './11-createEmployeesObject.js';
 export default function createReportObject(employeesList) {
   return {
     allEmployees: {
-      employeesList,
+      ...employeesList,
     },
     getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
 
   };
 }
+
+const employees = {
+  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+  ...createEmployeesObject('marketing', ['Sylvie'])
+};
+
+const report = createReportObject(employees);
+console.log(report.allEmployees);
+console.log(report.getNumberOfDepartments(report.allEmployees));
